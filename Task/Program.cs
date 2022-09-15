111908cb01b9daa903542712fd0b6475f448de5b
Christina Buramenskaya <christina.buramenskaya@gmail.com>
2022-09-16 01:56:43 +0300

    Task

---
+++ Task/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите элементы массива через запятую");
+string element = Console.ReadLine();
+string[] array = element.Split(',');
+int max_symbols = 3;
+Console.Write("[");
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (i < array.Length - 1)
+    {
+        if (array[i].Length <= max_symbols)
+        {
+            Console.Write($"{array[i]}, ");
+        }
+    }
+    else if (array[i].Length <= max_symbols)
+    {
+        Console.Write($"{array[i]}");
+    }
+}
+Console.Write("]");
